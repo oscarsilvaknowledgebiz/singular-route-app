9659c120a1afb4d7be1436e9d10c3798e4ec15bc
Ruben <ruben.carvalhas@knowledgebiz.pt>
2023-06-05 09:23:22 +0100

    Need to finish

--- App.js
+++ App.js
@@ -5,19 +5,24 @@ import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './src/view/WelcomeScreen';
 import Register from './src/view/Register';
 import Login from './src/view/Login';
+import HiAgain from './src/view/HiAgain';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='WelcomeScreen' screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName='HiAgain' screenOptions={{ headerShown: false }}>
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="Register" component={Register} options={{
           headerShown: true,
           headerTitle: ""
         }} />
         <Stack.Screen name="Login" component={Login} options={{
+          headerShown: true,
+          headerTitle: ""
+        }} />
+        <Stack.Screen name="HiAgain" component={HiAgain} options={{
           headerShown: true,
           headerTitle: ""
         }} />
